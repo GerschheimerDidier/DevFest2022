3b3b10c42f44d642c9efde9e0a37f787d0815e24
a.georgeon <a.georgeon@technologyandstrategy.com>
2022-10-11 12:31:49 +0200

    Allowance file

---
+++ truffle/contracts/Allowance.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17 < 0.9.0;
+
+contract Allowance {
+    address[] public allowance;
+    address private owner;
+
+    event AllowanceChanged(address indexed _forWho, address indexed _byWhom, uint64 _oldAmount, uint64 _newAmount);
+
+    mapping(address => uint64) public accountBeneficiary;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // - Only the owner can be send the ether on this smart contract
+    receive () external payable {
+        require(isOwner(), "You are not the owner of this wallet !");
+    }
+
+    modifier ownerOrAllowed(address _who)  {
+        require(isOwner() || accountBeneficiary[_who] > 0, "You are not the owner or this address is already on this wallet, please update it");
+        _;
+    }
+
+    function isOwner() internal view returns(bool) {
+        return owner == msg.sender;
+    }
+
+    function addAllowance(address _who, uint64 _amount) public ownerOrAllowed(_who) {
+        emit AllowanceChanged(_who, msg.sender, accountBeneficiary[_who], _amount);
+        allowance.push(_who);
+        accountBeneficiary[_who] = _amount;
+    }
+
+    function updateAllowance(address _who, uint64 _newAmount) public ownerOrAllowed(_who) {
+        emit AllowanceChanged(_who, msg.sender, accountBeneficiary[_who], _newAmount);
+
+        // - If new amount is 0 so the beneficiary can be deleted
+        if(_newAmount == 0) {
+            delete accountBeneficiary[_who];
+            uint index = findIndexAllowance(_who);
+            delete allowance[index];
+        }
+        else {
+            accountBeneficiary[_who] = _newAmount;
+        }
+    }
+
+    // - Allow to find the index of allowed address
+    function findIndexAllowance(address _who) private view returns(uint) {
+        for (uint i = 0; i < allowance.length; i++) {
+            if (allowance[i] == _who) {
+                return i;
+            }
+        }
+        revert();
+    }
+}
