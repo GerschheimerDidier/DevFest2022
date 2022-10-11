224d998ebaf960187f29385ecedf6c3b8072048c
a.georgeon <a.georgeon@technologyandstrategy.com>
2022-10-11 16:14:10 +0200

    Allowance and Wallet contract - Allowance allow to add a person to shared wallet & Wallet can withdraw a money allowated

--- truffle/contracts/Allowance.sol
+++ truffle/contracts/Allowance.sol
@@ -1,38 +1,54 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.17 < 0.9.0;
 
 contract Allowance {
     address[] public allowance;
-    address public owner;
+    address private owner;
 
-    mapping(address => int64) public accountBeneficiary;
+    event AllowanceChanged(address indexed _forWho, address indexed _byWhom, uint _oldAmount, uint _newAmount);
+
+    mapping(address => uint) public accountBeneficiary;
 
     constructor() {
         owner = msg.sender;
     }
 
-    receive () external payable isOwner {}
-
-    modifier isAlreadyBeneficiary()  {
-        require(!(accountBeneficiary[msg.sender] > 0), "You are already on this wallet but you can modifier your allowance");
+    modifier ownerOrAllowed(address _who)  {
+        require(isOwner() || accountBeneficiary[_who] > 0, "You are not the owner or this address is already on this wallet, please update it");
         _;
     }
 
-    modifier isOwner{
-        require(owner == msg.sender, "You are not the owner of this wallet");
-        _;
+    function isOwner() internal view returns(bool) {
+        return owner == msg.sender;
     }
 
-    function addAllowance(address _who, int64 _amount) public isOwner {
+    function addAllowance(address _who, uint _amount) public ownerOrAllowed(_who) {
+        emit AllowanceChanged(_who, msg.sender, accountBeneficiary[_who], _amount);
         allowance.push(_who);
         accountBeneficiary[_who] = _amount;
     }
 
-    function updateAllowance(address _who, int64 _newAmount) public isOwner {
-        accountBeneficiary[_who] = _newAmount;
+    function updateAllowance(address _who, uint _newAmount) public ownerOrAllowed(_who) {
+        emit AllowanceChanged(_who, msg.sender, accountBeneficiary[_who], _newAmount);
+
+        // - If new amount is 0 so the beneficiary can be deleted
+        if(_newAmount == 0) {
+            delete accountBeneficiary[_who];
+            uint index = findIndexAllowance(_who);
+            delete allowance[index];
+        }
+        else {
+            accountBeneficiary[_who] = _newAmount;
+        }
     }
 
-    function removeAllowance(address _who) public isOwner {
-        delete accountBeneficiary[_who];
+    // - Allow to find the index of allowed address
+    function findIndexAllowance(address _who) private view returns(uint) {
+        for (uint i = 0; i < allowance.length; i++) {
+            if (allowance[i] == _who) {
+                return i;
+            }
+        }
+        revert();
     }
 }
---
+++ truffle/contracts/Wallet.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17 < 0.9.0;
+
+import "./Allowance.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract Wallet is Allowance {
+    using SafeMath for uint;
+
+    event MoneySpent(address indexed _beneficiary, uint _amount);
+    event MoneyReceive(address indexed _from, uint _amount);
+
+    function withdrawMoney(uint _amount) public ownerOrAllowed(msg.sender) {
+        require(accountBeneficiary[msg.sender] >= _amount, "not enough funds has been allowed for your account");
+        uint newAmount = accountBeneficiary[msg.sender].sub(_amount);
+
+        updateAllowance(msg.sender, newAmount);
+
+        payable(msg.sender).transfer(_amount);
+        emit MoneySpent(msg.sender, _amount);
+    }
+
+    // - Only the owner can be send the ether on this smart contract
+    receive () external payable {
+        emit MoneyReceive(msg.sender, msg.value);
+        require(isOwner(), "You are not the owner of this wallet !");
+    }
+}
