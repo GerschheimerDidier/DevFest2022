92934a52efc383f5cf030b997a093c8f6d1d65fd
a.georgeon <a.georgeon@technologyandstrategy.com>
2022-10-19 16:11:02 +0200

    Review change

--- truffle/contracts/Wallet.sol
+++ truffle/contracts/Wallet.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.17 < 0.9.0;
 import "./Allowance.sol";
 
 contract Wallet is Allowance {
-    event MoneySpent(address indexed _beneficiary, uint _amount);
-    event MoneyReceive(address indexed _from, uint _amount);
+    event MoneyWithdrawn(address indexed _beneficiary, uint _amount);
+    event MoneyDeposited(address indexed _from, uint _amount);
 
     function withdrawMoney(uint _amount) public ownerOrAllowed(msg.sender) {
         require(accountBeneficiary[msg.sender] >= _amount, "not enough funds has been allowed for your account");
@@ -17,7 +17,6 @@ contract Wallet is Allowance {
         emit MoneySpent(msg.sender, _amount);
     }
 
-    // - Only the owner can be send the ether on this smart contract
     receive () external payable {
         emit MoneyReceive(msg.sender, msg.value);
     }
