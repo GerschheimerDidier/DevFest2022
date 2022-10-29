2712b899e3e90e26a3644c2a5ec71945f3ce2c44
a.georgeon <a.georgeon@technologyandstrategy.com>
2022-10-29 12:15:29 +0200

    advance

--- client/src/pages/SharedWallet/SharedWallet.jsx
+++ client/src/pages/SharedWallet/SharedWallet.jsx
@@ -1,27 +1,39 @@
 import { useEth } from "../../contexts/EthContext";
 import web3 from "web3"
+import {useState} from "react";
 
 const SharedWallet = () => {
 
-    // Permet de récupérer l'abi du smart contract
-    // Permet de récupérer l'address du wallet de la personne
-    const { contract, account } = useEth();
-    // console.log("contracts => ",contract)
-    let allowanceNumber = "";
+    //*********** USE STATE ***********//
+    const [allowanceAddr, setAllowanceAddr] = useState(0);
 
-    function getAllowanceIndex() {
-        // const addr = "0x4fe493bE9D13C464329558487B951b1817ed9151";
-        // allowanceNumber = contract.methods.accountBeneficiary(addr);
-        // allowanceNumber.then((res) => {
-        //     console.log(res.value);
-        // });
+    /*
+    * desc => contract is instance of contract. He contains method, abi, ...
+    * desc => account is addr of wallet connected with application
+     */
+    const { contract, account } = useEth();
+
+
+    async function getAllowanceWithAddr(){
+        const addr = "0x4fe493bE9D13C464329558487B951b1817ed9151";
+        try {
+            setAllowanceAddr(
+                web3.utils.fromWei(
+                    await contract.methods.accountBeneficiary(addr).call(),
+                    'ether'
+                )
+            );
+        }
+        catch (err) {
+            console.log(err)
+        }
     }
 
     function addAllowance() {
         try {
             contract.methods.defineAllowance(
                 "0x4fe493bE9D13C464329558487B951b1817ed9151",
-                web3.utils.toWei('2', 'ether'),
+                web3.utils.toWei('0.002', 'ether'),
             ).send({
                 from: account[0],
             });
@@ -32,13 +44,42 @@ const SharedWallet = () => {
         }
     }
 
+    function giveMyMoney() {
+        try {
+            contract.methods.withdrawMoney(web3.utils.toWei('0.002', 'ether'))
+                .send({
+                    from: account[0],
+                });
+
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    function sendMoney() {
+        try {
+            contract.send(web3.utils.toWei('0.003', 'ether'))
+            //     {
+            //     from: account[0],
+            //     value: web3.utils.toWei('2', 'ether')
+            // });
+        }
+        catch (err) {
+            console.log(err)
+        }
+
+    }
+
     return (
         <div>
-            <h2>Le Shared wallet</h2>
-            <p> Allowance price => { allowanceNumber }</p>
-            <button onClick={ getAllowanceIndex }>getAllowanceZero</button>
+            <h2 className={"text-3xl font-bold underline"}>Le Shared wallet</h2>
+            <p> Allowance price => { allowanceAddr } ETH</p>
+            <button onClick={ getAllowanceWithAddr }>getAllowanceWithAddr</button>
 
             <button onClick={ addAllowance } type={"button"}>Ajout Allowance</button>
+            <button onClick={ sendMoney } type={"button"}>Send Money on contract</button>
+            <button onClick={ giveMyMoney } type={"button"}>Withdraw my money</button>
         </div>
 
 
